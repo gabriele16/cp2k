74665d620c33ff8e36d3a967086a50cc8e7349da
gabriele16 <gabrielet85@gmail.com>
2025-03-31 16:01:14 +0200

    fix typos and install script

--- colab/install_cuda.sh
+++ colab/install_cuda.sh
@@ -1,7 +1,10 @@
 #!/bin/bash -e
 
 # Download the repository pin file
+# Set noninteractive mode for apt
+! export DEBIAN_FRONTEND=noninteractive
 
+# Download the repository pin file
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
 sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
 
